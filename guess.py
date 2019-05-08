d9fbcc9d731e76fa2ecfdde0ce3579a8b71e4f96
WEN Elvis <Elvis.WEN@oxinst.com>
2019-05-08 22:00:00 +0800

    Add guessing chances

--- guess.py
+++ guess.py
@@ -2,11 +2,23 @@
 
 from random import randint
 print "Guessing game starts."
+print "You have five chances."
 num=randint(1,10)
+i=1
 while True:
     guess=input("Please guess the number [1,10]: ")
-    if guess!=num:
-        print "Please guess the number again."
+    if i<5:
+        if guess!=num:
+            print "Please guess the number again."
+            i+=1
+        else:
+            print "That's right. Well done!"
+            break
     else:
-        print "That's right. Well done!"
+        if guess!=num:
+            print "Sorry, you have reached the maximal chance."
+            print "The number is {}.".format(num)
+        else:
+            print "That's right. Well done!"
         break
+    
